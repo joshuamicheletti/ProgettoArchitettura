1ffb2a8ee727cfa6f557bb543a226f7821205710
Joshua Micheletti <j.micheletti@campus.uniurb.it>
2021-12-03 18:53:33 +0100

    sistemato il codice, aggiunti pattern di prova, tradotto il codice da inglese a italiano

--- GameOfLife.s
+++ GameOfLife.s
@@ -6,26 +6,28 @@
     y:        .byte 0
     color:    .byte 30, 144, 255, 0
 
-    row0:     .byte 1,0,1,0,0,0,0,1,0,1,0,1,1,1,0,1
-    row1:     .byte 1,0,0,1,0,0,1,1,0,0,0,0,0,0,1,0
-    row2:     .byte 1,1,1,0,1,1,1,1,0,0,0,0,1,1,0,0       
-    row3:     .byte 0,0,1,1,0,0,1,0,1,1,0,0,1,0,1,1
-    row4:     .byte 1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1
-    row5:     .byte 0,1,0,0,0,1,0,1,0,1,0,0,0,0,0,1
-    row6:     .byte 1,1,1,0,1,1,0,1,1,0,1,1,0,0,1,1
-    row7:     .byte 0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,1
-    row8:     .byte 1,1,0,1,1,0,1,0,1,0,0,0,0,1,1,0
-    row9:     .byte 0,1,1,1,1,0,1,0,1,0,0,1,1,1,0,0
-    row10:    .byte 0,0,0,1,1,1,0,1,1,1,0,1,1,0,0,0
-    row11:    .byte 0,0,1,0,1,1,1,1,0,0,0,1,1,1,1,0
-    row12:    .byte 1,1,0,0,0,0,1,1,1,0,0,1,1,0,1,0
-    row13:    .byte 0,1,0,0,1,1,1,1,0,1,0,1,0,1,0,1
-    row14:    .byte 0,1,1,0,1,0,1,1,0,0,1,0,0,1,0,0
-    row15:    .byte 0,1,0,1,0,0,0,0,1,0,0,1,1,1,1,1
+    # random board
+    # row0:     .byte 1,0,1,0,0,0,0,1,0,1,0,1,1,1,0,1
+    # row1:     .byte 1,0,0,1,0,0,1,1,0,0,0,0,0,0,1,0
+    # row2:     .byte 1,1,1,0,1,1,1,1,0,0,0,0,1,1,0,0
+    # row3:     .byte 0,0,1,1,0,0,1,0,1,1,0,0,1,0,1,1
+    # row4:     .byte 1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1
+    # row5:     .byte 0,1,0,0,0,1,0,1,0,1,0,0,0,0,0,1
+    # row6:     .byte 1,1,1,0,1,1,0,1,1,0,1,1,0,0,1,1
+    # row7:     .byte 0,0,0,0,1,0,1,0,1,1,1,1,1,1,1,1
+    # row8:     .byte 1,1,0,1,1,0,1,0,1,0,0,0,0,1,1,0
+    # row9:     .byte 0,1,1,1,1,0,1,0,1,0,0,1,1,1,0,0
+    # row10:    .byte 0,0,0,1,1,1,0,1,1,1,0,1,1,0,0,0
+    # row11:    .byte 0,0,1,0,1,1,1,1,0,0,0,1,1,1,1,0
+    # row12:    .byte 1,1,0,0,0,0,1,1,1,0,0,1,1,0,1,0
+    # row13:    .byte 0,1,0,0,1,1,1,1,0,1,0,1,0,1,0,1
+    # row14:    .byte 0,1,1,0,1,0,1,1,0,0,1,0,0,1,0,0
+    # row15:    .byte 0,1,0,1,0,0,0,0,1,0,0,1,1,1,1,1
 
+    # glider
     # row0:     .byte 0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    # row0:     .byte 0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0
-    # row2:     .byte 1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0       
+    # row1:     .byte 0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row2:     .byte 1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0
     # row3:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
     # row4:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
     # row5:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
@@ -40,433 +42,443 @@
     # row14:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
     # row15:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
-    altRow0:  .byte 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
-    altRow1:  .byte 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
-    altRow2:  .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow3:  .byte 1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0
-    altRow4:  .byte 1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0
-    altRow5:  .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow6:  .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow7:  .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow8:  .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow9:  .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow10: .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow11: .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow12: .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow13: .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow14: .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    altRow15: .byte 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # fireworks
+    # row0:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row1:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row2:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row3:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row4:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row5:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row6:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row7:     .byte 0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0
+    # row8:     .byte 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0
+    # row9:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row10:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row10:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row12:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row13:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row14:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    # row15:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
+    # alt glider
+    row0:     .byte 1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0
+    row1:     .byte 0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0
+    row2:     .byte 1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0
+    row3:     .byte 0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0
+    row4:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row5:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row6:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row7:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row8:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row9:     .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row10:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row10:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row12:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row13:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row14:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    row15:    .byte 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
     
 .text
-    lwu r30, CONTROL(r0)
-    lwu r31, DATA(r0)
+                                            ; r0 = registro contenente il valore 0
+    daddi r1, r0, 1                         ; r1 = registro contenente il valore 1
 
-    lbu r1, x(r0)
-    lbu r2, y(r0)
-    lwu r3, color(r0)
+                                            ; r2 = indirizzo in memoria dei dati della tavola di origine
+    daddi r3, r0, 256                       ; r3 = indirizzo in memoria dei dati della tavola di destinazione
+                                            ; (offset tra r2 e r3 = 256)
+                                            ; r4 = indirizzo in memoria dei vicini ai dati della tavola di origine
 
-    sb r1, 5(r31)
-    sb r2, 4(r31)
-    sw r3, 0(r31)
+                                            ; r5 = valore della cella considerata
+                                            ; r6 = numero di vicini alla cella considerata
 
-    daddi r29, r0, 5
-    #sd r29, (r30)
-
-    daddi r4, r0, 16
-    daddi r5, r0, 15
+    lbu r10, x(r0)                          ; r10 = valore di x per disegnare
+    lbu r11, y(r0)                          ; r11 = valore di y per disegnare
     
-    # lbu r8, byteAlive(r0)
+    daddi r12, r0, 16                       ; r12 = valore di iteratore per x (inizializzato a 16)
+    daddi r13, r0, 15                       ; r13 = valore di iteratore per y (inizializzato a 15)
 
-    # sb r8, altRow7(r0)
+    daddi r15, r0, 15                       ; r15 = valore di limite superiore di y [0, 15] [r0, r15]
+    daddi r16, r0, 16                       ; r16 = valore di limite superiore di x [1, 16] [r1, r16]
 
-    # daddi r8, r0, 2
+    daddi r17, r0, 3                        ; r17 = numero di vicini necessari per far nascere una cella
 
-    # daddi r7, r0, 256
+    lwu r28, color(r0)                      ; r28 = colore con cui disegnare
+    daddi r29, r0, 5                        ; r29 = valore per determinare il funzionamento del
+                                            ; terminale (5 = disegna terminale grafico)
 
-    # sb r8, row0(r7)
+    lwu r30, CONTROL(r0)                    ; r30 = registro contenente l indirizzo della area in
+                                            ; memoria contenente la modalita di utilizzo del terminale
+                                            ; 5 - disegna a schermo
+                                            ; 6 - pulisce il terminale testuale
+                                            ; 7 - pulisce il terminale grafico
+                                            ; 8 - legge un byte dal terminale
 
-    # daddi r7, r0, 0
-
-    # j checkAlive
-
-    daddi r9, r0, 16
-    daddi r14, r0, 15
-
-    daddi r16, r0, 3
-
-    daddi r12, r0, 1
-
-    daddi r20, r0, 256
-
-    # j calculateNeighbors
-    j checkAlive
-
-newLineNeighbors:
-    daddi r5, r5, -1
-    daddi r4, r0, 16
-
-calculateNeighbors:
-    lb r6, row0(r7)
-    
-    beq r4, r9, case187
-    beq r4, r12, case345
-    
-    beq r5, r14, case123
-    beq r5, r0, case765
-
-    #1
-    daddi r11, r7, -17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #2
-    daddi r11, r7, -16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #3
-    daddi r11, r7, -15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #4
-    daddi r11, r7, 1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #5
-    daddi r11, r7, 17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #6
-    daddi r11, r7, 16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #7
-    daddi r11, r7, 15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #8
-    daddi r11, r7, -1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-update:
-    # sb r13, altRow0(r7)
-
-    daddi r13, r0, 0
-
-    daddi r7, r7, 1
-    daddi r20, r20, 1
-    daddi r4, r4, -1
-
-    bnez r4, calculateNeighbors
-    bnez r5, newLineNeighbors
-
-    # daddi r29, r0, 8
-    # sb r29, (r30)
-    # daddi r29, r0, 5
-
-    daddi r4, r0, 16
-    daddi r5, r0, 15
-    daddi r7, r7, -256
-    daddi r20, r20, -256
-
-    j switchBoard
-    halt
-
-case345:
-    beq r5, r14, case3 ;case345
-    beq r5, r0, case5
-
-    #case 4
-    #r11 = r7 - 17
-    # 1
-    daddi r11, r7, -17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    # 2
-    daddi r11, r7, -16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    # 6
-    daddi r11, r7, 16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    # 7
-    daddi r11, r7, 15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    # 8
-    daddi r11, r7, -1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case5:
-    # 1
-    daddi r11, r7, -17 ;case5
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-     #2
-    daddi r11, r7, -16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #8
-    daddi r11, r7, -1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case3:
-    #6
-    daddi r11, r7, 16 ;case3
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #7
-    daddi r11, r7, 15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #8
-    daddi r11, r7, -1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case187:
-    beq r5, r14, case1 ;case187
-    beq r5, r0, case7
-    # case8
-    #2
-    daddi r11, r7, -16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #3
-    daddi r11, r7, -15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #4
-    daddi r11, r7, 1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #5
-    daddi r11, r7, 17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #6
-    daddi r11, r7, 16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case7:
-    #2
-    daddi r11, r7, -16 ;case7
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #3
-    daddi r11, r7, -15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #4
-    daddi r11, r7, 1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case1:
-    #4
-    daddi r11, r7, 1 ;case1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #5
-    daddi r11, r7, 17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #6
-    daddi r11, r7, 16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case765:
-    beq r4, r9, case5 ;case765
-    beq r4, r12, case7
-    #case6
-    #1
-    daddi r11, r7, -17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #2
-    daddi r11, r7, -16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #3
-    daddi r11, r7, -15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #4
-    daddi r11, r7, 1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #8
-    daddi r11, r7, -1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
-
-case123:
-    beq r4, r9, case1 ;case123
-    beq r4, r12, case3
-    #case2
-    #4
-    daddi r11, r7, 1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #5
-    daddi r11, r7, 17
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #6
-    daddi r11, r7, 16
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #7
-    daddi r11, r7, 15
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-    #8
-    daddi r11, r7, -1
-    lb r10, row0(r11)
-    dadd r13, r13, r10
-
-    j rules
+    lwu r31, DATA(r0)                       ; r31 = registro contenente l indirizzo della area
+                                            ; in memoria contenente i dati da rappresentare a terminale
 
 
-rules:
-    lb r15, row0(r7) ;rules
-    
-    beqz r15, deadRules
+    j controllaStato
 
-    beqz r13, die
+nuovaRiga: 
+    daddi r13, r13, -1  ;nuovaRiga
+    daddi r12, r0, 16
 
-    daddi r13, r13, -1
+    daddi r11, r11, 1
+    daddi r10, r0, 0
 
-    beqz r13, die
-
-    daddi r13, r13, -1
-
-    beqz r13, live
-
-    daddi r13, r13, -1
-
-    beqz r13, live
-
-    sb r0, row0(r20)
-
-    j update
-
-deadRules:
-    beq r13, r16, born ;dead rules
-    sb r0, row0(r20)
-    j update
-
-born:
-    sb r12, row0(r20) ;born
-    j update
-
-die:
-    sb r0, row0(r20) ;die
-    j update
-
-live:
-    sb r12, row0(r20) ;live
-    j update 
-
-
-
-drawResult:
-newLine: 
-    daddi r5, r5, -1  ;newLine
-    daddi r4, r0, 16
-
+controllaStato: 
+    lb r5, row0(r2) ;controllaStato
     daddi r2, r2, 1
-    daddi r1, r0, 0
 
-checkAlive: 
-    lb r6, row0(r7) ;checkAlive
-    daddi r7, r7, 1
+    bnez r5, vivo
+    j morto
 
-    bnez r6, alive
-    j dead
+morto: 
+    daddi r10, r10, 1 ;morto
+    daddi r12, r12, -1
 
-dead: 
-    daddi r1, r1, 1 ;dead
-    daddi r4, r4, -1
+    bnez r12, controllaStato
+    bnez r13, nuovaRiga
 
-    bnez r4, checkAlive
-    bnez r5, newLine
+    daddi r2, r2, -256
 
-    daddi r7, r7, -256
+    daddi r12, r0, 16
+    daddi r13, r0, 15
 
-    daddi r4, r0, 16
-    daddi r5, r0, 15
+    j calcolaVicini
 
-    j calculateNeighbors
-
-alive: 
-    sw r3, 0(r31) ;alive
-    sb r1, 5(r31)
-    sb r2, 4(r31)
+vivo: 
+    sw r28, 0(r31) ; vivo
+    sb r10, 5(r31) 
+    sb r11, 4(r31)
 
     sd r29, (r30)
 
-    daddi r1, r1, 1
-    daddi r4, r4, -1
+    daddi r10, r10, 1
+    daddi r12, r12, -1
 
-    bnez r4, checkAlive
-    bnez r5, newLine
+    bnez r12, controllaStato
+    bnez r13, nuovaRiga
 
-    daddi r7, r7, -256
+    daddi r2, r2, -256
 
-    daddi r4, r0, 16
-    daddi r5, r0, 15
+    daddi r12, r0, 16
+    daddi r13, r0, 15
 
-    j calculateNeighbors
+    j calcolaVicini
 
 
-switchBoard:
+
+nuovaRigaVicini:
+    daddi r13, r13, -1
+    daddi r12, r0, 16
+
+calcolaVicini: 
+    beq r12, r16, caso187
+    beq r12, r1, caso345
+    
+    beq r13, r15, caso123
+    beq r13, r0, caso765
+
+    #1
+    daddi r4, r2, -17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #2
+    daddi r4, r2, -16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #3
+    daddi r4, r2, -15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #4
+    daddi r4, r2, 1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #5
+    daddi r4, r2, 17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #6
+    daddi r4, r2, 16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #7
+    daddi r4, r2, 15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #8
+    daddi r4, r2, -1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+aggiorna:
+    # sb r6, altRow0(r2)
+
+    daddi r6, r0, 0
+
+    daddi r2, r2, 1
+    daddi r3, r3, 1
+    daddi r12, r12, -1
+
+    bnez r12, calcolaVicini
+    bnez r13, nuovaRigaVicini
+
+    daddi r12, r0, 16
+    daddi r13, r0, 15
+    daddi r2, r2, -256
+    daddi r3, r3, -256
+
+    j scambiaTavola
+    halt
+
+caso345:
+    beq r13, r15, caso3 ;caso345
+    beq r13, r0, caso5
+
+    #caso 4
+    #r4 = r2 - 17
+    # 1
+    daddi r4, r2, -17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    # 2
+    daddi r4, r2, -16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    # 6
+    daddi r4, r2, 16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    # 7
+    daddi r4, r2, 15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    # 8
+    daddi r4, r2, -1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso5:
+    # 1
+    daddi r4, r2, -17 ;caso5
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+     #2
+    daddi r4, r2, -16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #8
+    daddi r4, r2, -1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso3:
+    #6
+    daddi r4, r2, 16 ;caso3
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #7
+    daddi r4, r2, 15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #8
+    daddi r4, r2, -1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso187:
+    beq r13, r15, caso1 ;caso187
+    beq r13, r0, caso7
+    # caso8
+    #2
+    daddi r4, r2, -16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #3
+    daddi r4, r2, -15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #4
+    daddi r4, r2, 1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #5
+    daddi r4, r2, 17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #6
+    daddi r4, r2, 16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso7:
+    #2
+    daddi r4, r2, -16 ;caso7
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #3
+    daddi r4, r2, -15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #4
+    daddi r4, r2, 1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso1:
+    #4
+    daddi r4, r2, 1 ;caso1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #5
+    daddi r4, r2, 17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #6
+    daddi r4, r2, 16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso765:
+    beq r12, r16, caso5 ;caso765
+    beq r12, r1, caso7
+    #caso6
+    #1
+    daddi r4, r2, -17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #2
+    daddi r4, r2, -16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #3
+    daddi r4, r2, -15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #4
+    daddi r4, r2, 1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #8
+    daddi r4, r2, -1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+caso123:
+    beq r12, r16, caso1 ;caso123
+    beq r12, r1, caso3
+    #caso2
+    #4
+    daddi r4, r2, 1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #5
+    daddi r4, r2, 17
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #6
+    daddi r4, r2, 16
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #7
+    daddi r4, r2, 15
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+    #8
+    daddi r4, r2, -1
+    lb r5, row0(r4)
+    dadd r6, r6, r5
+
+    j regole
+
+
+
+regole:
+    lb r5, row0(r2) ;regole
+    
+    beqz r5, regoleMorto
+
+    beqz r6, muori
+
+    daddi r6, r6, -1
+
+    beqz r6, muori
+
+    daddi r6, r6, -1
+
+    beqz r6, vivi
+
+    daddi r6, r6, -1
+
+    beqz r6, vivi
+
+    sb r0, row0(r3)
+
+    j aggiorna
+
+regoleMorto:
+    beq r6, r17, nasci ;dead rules
+    sb r0, row0(r3)
+    j aggiorna
+
+nasci:
+    sb r1, row0(r3) ;nasci
+    j aggiorna
+
+muori:
+    sb r0, row0(r3) ;muori
+    j aggiorna
+
+vivi:
+    sb r1, row0(r3) ;vivi
+    j aggiorna 
+
+
+
+scambiaTavola:
     daddi r29, r0, 8
     sd r29, (r30)
 
-    # daddi r29, r0, 6
-    # sd r29, (r30)
+    daddi r29, r0, 6
+    sd r29, (r30)
 
     daddi r29, r0, 7
     sd r29, (r30)
     daddi r29, r0, 5
 
-    daddi r4, r0, 16
-    daddi r5, r0, 15
+    daddi r12, r0, 16
+    daddi r13, r0, 15
 
-    daddi r1, r0, 0
+    daddi r10, r0, 0
+    daddi r11, r0, 0
+
+    bnez r2, tavolaAltATavola
+    j tavolaATavolaAlt
+
+tavolaATavolaAlt:
+    daddi r2, r0, 256
+    daddi r3, r0, 0
+    j controllaStato
+
+tavolaAltATavola:
     daddi r2, r0, 0
-
-    bnez r7, altBoardToBoard
-    j boardToAltBoard
-
-boardToAltBoard:
-    daddi r7, r0, 256
-    daddi r20, r0, 0
-    j checkAlive
-
-altBoardToBoard:
-    daddi r7, r0, 0
-    daddi r20, r0, 256
-    j checkAlive
+    daddi r3, r0, 256
+    j controllaStato
